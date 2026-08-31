e88e8cf78f7b07fbc635c340b8cbace8454b0946
RamPrasad-6147 <rajeshrambala15@gmail.com>
2026-08-31 13:10:25 +0530

    commit after update3

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://login-signup-frontend-update1.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
